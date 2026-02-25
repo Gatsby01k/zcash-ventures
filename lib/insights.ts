3779fe6a43e953125fde12a2a95212eca302856c
Gatsby01k <vladyslavddos@gmail.com>
2026-02-25 20:26:11 +0200

    Create insights.ts

---
+++ lib/insights.ts
@@ -0,0 +1,14 @@
+export const insights = [
+  {
+    slug: "what-is-a-zcash-otc-desk",
+    lastModified: "2026-02-20",
+  },
+  {
+    slug: "otc-vs-exchange",
+    lastModified: "2026-02-20",
+  },
+  {
+    slug: "market-impact-and-zec",
+    lastModified: "2026-02-20",
+  },
+];
